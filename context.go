8e0f733dcf89b61227a0e790f85ca805a492b2f5
glebkin <gleb.kogtev@gmail.com>
2024-01-18 17:07:26 +0300

    Add default clock and nil context check

--- context.go
+++ context.go
@@ -6,6 +6,9 @@ import (
 	"github.com/itbasis/go-clock/v2/pkg"
 )
 
+// Default holds real clock implementation
+var Default = New()
+
 // Used as a context key which holds clock value
 type ctxClock struct{}
 
@@ -15,11 +18,14 @@ func WithContext(ctx context.Context, clock pkg.Clock) context.Context {
 }
 
 // FromContext returns the implementation of clock associated with provided context.
-// It returns new instance of real clock implementation if not present.
+// It returns default implementation if not present.
 func FromContext(ctx context.Context) pkg.Clock {
+	if ctx == nil {
+		panic("nil context passed to Clock")
+	}
 	if clock, ok := ctx.Value(ctxClock{}).(pkg.Clock); ok {
 		return clock
 	}
 
-	return New()
+	return Default
 }
